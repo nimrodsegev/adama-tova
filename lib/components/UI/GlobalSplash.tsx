47a736b049b396b4ea23c790d990f0e947af205f
nadavnavon <nadav.navon2@mail.huji.ac.il>
2026-01-18 14:11:03 +0200

    add 0.6 sec till the text appear in splash screen

--- lib/components/UI/GlobalSplash.tsx
+++ lib/components/UI/GlobalSplash.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import OrganicCircles from "@/lib/components/OrganicCircles/OrganicCircles";
 
 // ⏱️ CONTROL SETTINGS
-const TEXT_APPEAR_DELAY = 2600;
+const TEXT_APPEAR_DELAY = 3200;
 const EXTRA_HOLD_TIME = 1000;
 
 export default function GlobalSplash() {
